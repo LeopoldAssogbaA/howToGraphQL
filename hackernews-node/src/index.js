6f4301b18b5bf15524993ac35f900f8b14c1f4ab
Léopold <leopoldassogba21@gmail.com>
2020-12-15 12:35:45 +0100

    connecting server & db

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -2,99 +2,69 @@ const fs = require('fs');
 const path = require('path');
 
 const { ApolloServer } = require('apollo-server');
-
-// const typeDefs = `
-//   type Query {
-//     info: String!
-//     feed: [Link!]!
-//   }
-
-//   type Mutation {
-//     post(url: String!, description: String!): Link!
-//   }
-
-//   type Link {
-//     id: ID!
-//     description: String!
-//     url: String!
-//   }
-// `
-
-// 1
-let links = [
-    {
-        id: 'link-0',
-        url: 'www.howtographql.com',
-        description: 'Fullstack tutorial for GraphQL'
-    },
-    {
-        id: 'link-1',
-        url: 'www.howtographql.com',
-        description: 'Fullstack tutorial for GraphQL 1'
-    },
-    {
-        id: 'link-2',
-        url: 'www.howtographql.com',
-        description: 'Fullstack tutorial for GraphQL 2'
-    },
-];
-
-let idCount = links.length;
+const { PrismaClient } = require('@prisma/client')
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         // 2
-        feed: () => links,
+        feed: async (parent, args, context) => {
+            return context.prisma.link.findMany()
+        },
     },
 
     Mutation: {
-        // 2
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            links.push(link)
-            return link
+        post: (parent, args, context, info) => {
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            })
+            return newLink
         },
-        updateLink: (parent, args) => {
-            const linksSorted = links.filter(links => links.id !== args.id);
-            const updatedLink = {
-                id: updatedLink.id,
-                url: args.url ? args.url : updatedLink.url,
-                description: args.description ? args.description : updatedLink.description,
-            };
-            
-            links = linksSorted.push(updatedLink);
-            
-            return updatedLink;
-        },
-        deleteLink: (parent, args) => {
-            const linkDeleted = links.find(link => links.id === args.id);
-            links = links.filter(links => links.id !== args.id);
 
-            return linkDeleted;
-        }
+        // updateLink: (parent, args) => {
+        //     const linksSorted = links.filter(links => links.id !== args.id);
+        //     const updatedLink = {
+        //         id: updatedLink.id,
+        //         url: args.url ? args.url : updatedLink.url,
+        //         description: args.description ? args.description : updatedLink.description,
+        //     };
+
+        //     links = linksSorted.push(updatedLink);
+
+        //     return updatedLink;
+        // },
+        // deleteLink: (parent, args) => {
+        //     const linkDeleted = links.find(link => links.id === args.id);
+        //     links = links.filter(links => links.id !== args.id);
+
+        //     return linkDeleted;
+        // }
 
     },
-        // 3
-        Link: {
-            id: (parent) => parent.id,
-            description: (parent) => parent.description,
-            url: (parent) => parent.url,
-        }
-    }
+    // 3
+    //     Link: {
+    //         id: (parent) => parent.id,
+    //         description: (parent) => parent.description,
+    //         url: (parent) => parent.url,
+};
+
+
+const prisma = new PrismaClient()
 
 const server = new ApolloServer({
-        typeDefs: fs.readFileSync(
-            path.join(__dirname, 'schema.graphql'),
-            'utf8'
-        ),
-        resolvers,
-    });
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
+    context: {
+        prisma,
+    },
+    resolvers,
+});
 
-    server
+server
     .listen()
-        .then(({ url }) => console.log(`server is running on ${url}`));
+    .then(({ url }) => console.log(`server is running on ${url}`));
